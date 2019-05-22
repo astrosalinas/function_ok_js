7fed476e4ef1b05b70a070984319cd844e2d3429
astrosalinas <astrosalinas35@gmail.com>
2019-05-22 08:38:02 -0400

    function searh in array

--- funcitons.js
+++ funcitons.js
@@ -1,5 +1,14 @@
 // delete repeated elements in array
-let arr = [1,1,2,2,3,3]
-let new_arr = arr.filter(
-  (value, index) => arr.indexOf(value) >= index
-)
+let arr = [1, 1, 2, 2, 3, 3];
+let new_arr = arr.filter((value, index) => arr.indexOf(value) >= index);
+
+//searh in array
+search_in_array = () => {
+  let items = ["palo", "pelo", "hola", "sape"];
+  var test = "palo";
+  var updatedList = items;
+  updatedList = updatedList.filter(item => {
+    return item.toLowerCase().search(test.toLowerCase()) !== -1;
+  });
+  return updatedList;
+};
